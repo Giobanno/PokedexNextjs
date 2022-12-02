75e4c904573f22c853d213c86e10ef6797b2db1b
Giovanni <giovanni@dogger.nl>
2022-12-02 20:30:47 +0100

    detail page fetching

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Card = ({ pokemon }) => {
+
   return (
-    <div className="flex flex-col justify-center items-center bg-slate-800">
-      <Image
-        src={pokemon.sprites.back_default}
-        alt={pokemon.name}
-        width={75}
-        height={75}
-      />
-      <div className="text-center">
-        {pokemon.name}
-      </div>
-    </div>
+    <>
+      <Link 
+        href={{pathname: "/detail", query: {id: pokemon.id} }}
+      >
+        <div className="flex flex-col justify-center items-center bg-slate-800">
+          <Image
+            src={pokemon.sprites.back_default}
+            alt={pokemon.name}
+            width={75}
+            height={75}
+          />
+          <div className="text-center">
+            {pokemon.name}
+          </div>
+        </div>
+      </Link>
+    </>
+
   )
 }
 
